34c29f21577057360d0996c4137960514ade9865
Tyler Letendre <tylerletendre98@gmail.com>
2022-09-19 10:17:41 -0400

    figure out why the search citeria wasnt working now it gives error if username or email has already been used

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -6,20 +6,21 @@ router.post('/newUser', async(req,res)=>{
     try {
         console.log(req.body)
         const user = await User.findOne({$or:[{username:req.body.username},{email:req.body.email}]})
-        if(user !== null){
+        if(user === null){
             console.log(user)
-            return res.status(400).send('Username already exists')
-        }else{
-            const newUser = {
+            const newUser = new User({
                 username:req.body.username,
-                email:req.body.body.email,
                 password:req.body.password,
+                email:req.body.email,
                 dob:req.body.dob,
                 firstname:req.body.firstname,
                 lastname:req.body.lastname
-            }
+            })
             newUser.save()
             return res.send(newUser)
+        }else{
+            console.log(user)
+            return res.status(400).send("Username or Email already in use")
         }
     } catch (ex) {
         return res.status(500).send(`Internal Server Error ${ex}.`)
